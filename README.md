6e5c6519d2c41160dca8e9a5f4d6431ef0ed334d
David K. Bainbridge <dbainbri@ciena.com>
2014-10-14 21:56:34 -0700

    initial migration into ODL repo
    Change-Id: If26febd29f90a156ca65e865a6fb66d4fa822036
    Signed-off-by: David K. Bainbridge <dbainbri@ciena.com>

---
+++ README.md
@@ -0,0 +1,32 @@
+Discovery Module
+================
+This module provides the model and default implementations to support discovery, identification, synchronization, 
+and connection (link) resolution of network elements under the OpenDaylight (ODL) controller. The definition of these 
+capabilities for the purpose of this module are:
+
+* __discovery__ - inform the lifecycle processing of the existance of a network element
+* __identification__ - a discovered network element is associated with an OpenDaylight (ODL) node type, such that RPCs
+routed based on this node type will provide node type specific implementations.
+* __synchronization__ - an identified network element's configuration is resolved or synchronized into the OpenDaylight
+information model
+* __connection resolution__ - connections or links between synchronized network elements are identified and stored into
+the OpenDaylight information model
+
+General Flow
+------------
+The general flow of the discovery process is that when a network element is discovered a notification is published
+that is "handled" by each network element support module. The module attempts to connect to the network element and if
+it is successful it publishes a network element identified notification indicating the node type to be associated with
+the network element.
+
+The notifications of identified network elements are handled by a synchronization module which queues up a request to
+synchronize the network element. These requests are executed against a thread pool. When the synchronization request
+is activated a routed RPC, "synchronize" is called against the network element. This request is handled by the 
+network element support module and should perform a deep inspection of the device and update the OpenDaylight 
+inventory appropriately. When a synchronization is complete the synchronization module will publish a synchronized
+notification.
+
+The synchronized notification will be handled by the connection resolution module. This module will evaluate the known
+information about the network and based on any additional information about a network element that was synchronized it
+may create, delete, or update the network element connections in the OpenDaylight information model.
+
---
+++ provider/lifecycle/src/main/java/org/opendaylight/lifecycle/providers/synchronization/SynchronizationProvider.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2014 Ciena Corporation and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.lifecycle.providers.synchronization;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lifecycle.identification.rev140714.LifecycleIdentificationListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lifecycle.identification.rev140714.NetworkElementIdentified;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lifecycle.identification.rev140714.NetworkElementInProcess;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lifecycle.identification.rev140714.UnableToIdentifyNetworkElement;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lifecycle.synchronization.rev140714.LifecycleSynchronizationListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lifecycle.synchronization.rev140714.NetworkElementSynchronizationFailure;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lifecycle.synchronization.rev140714.NetworkElementSynchronized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * OSGi/ODL Activator that registers handlers for notifications pertaining to network elements being identified. If a
+ * network element is identified then the handler queues up a request to synchronize the network element. If a network
+ * element fails to be identified then an error message is logged.
+ *
+ * @author David Bainbridge <dbainbri@ciena.com>
+ * @since 2014-07-15
+ */
+public class SynchronizationProvider implements LifecycleIdentificationListener, LifecycleSynchronizationListener,
+        AutoCloseable {
+    private static Logger log = LoggerFactory.getLogger(SynchronizationProvider.class);
+    private final ExecutorService executor;
+    private final NotificationProviderService notificationProviderService;
+    private final RpcProviderRegistry rpcRegistry;
+
+    public SynchronizationProvider(NotificationProviderService notificationProviderService,
+            RpcProviderRegistry rpcRegistry, int threadPoolSize) {
+        this.notificationProviderService = notificationProviderService;
+        this.rpcRegistry = rpcRegistry;
+        log.debug("CREATE : {} : SIZE = {}", Integer.toHexString(this.hashCode()), threadPoolSize);
+        executor = Executors.newFixedThreadPool(threadPoolSize);
+    }
+
+    @Override
+    public void onNetworkElementIdentified(NetworkElementIdentified notification) {
+        /*
+         * A new network element has been discovered (existence) and has been identified as a known node type. At this
+         * point it needs to be queued up for synchronization.
+         */
+        log.debug("EVENT : NetworkElementIdentified : RECEIVED : {}, {}, {}", notification.getRequestId(),
+                notification.getNetworkElementIp(), notification.getNetworkElementType());
+        executor.submit(new SynchronizationJob(rpcRegistry, notificationProviderService, notification));
+        log.debug("JOB : synchronization : SUMMITED : {}, {}, {}", notification.getRequestId(),
+                notification.getNetworkElementIp(), notification.getNetworkElementType());
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.opendaylight.yang.gen.v1.urn.opendaylight.lifecycle.identification.rev140714.IdentificationListener#
+     * onNetworkElementInProcess
+     * (org.opendaylight.yang.gen.v1.urn.opendaylight.lifecycle.identification.rev140714.NetworkElementInProcess)
+     */
+    @Override
+    public void onNetworkElementInProcess(NetworkElementInProcess arg0) {
+        // no op
+    }
+
+    @Override
+    public void onUnableToIdentifyNetworkElement(UnableToIdentifyNetworkElement notification) {
+        log.debug("EVENT : UnableToIdentifyNetworkElement : RECEIVED : {}, {}", notification.getRequestId(),
+                notification.getNetworkElementIp());
+        log.error("Unable to associate network element ({}) with controller node type. CAUSE: {}",
+                notification.getNetworkElementIp(), notification.getCause());
+    }
+
+    @Override
+    public void onNetworkElementSynchronizationFailure(NetworkElementSynchronizationFailure notification) {
+        log.debug("EVENT : NetworkElementSynchronizationFailure : RECEIVED : {}, {}, {}", notification.getRequestId(),
+                notification.getNetworkElementIp(), notification.getNetworkElementType());
+        log.error("Error when attempting to synchronize network element {} using request ID {}. CAUSE {}",
+                notification.getNetworkElementIp(), notification.getRequestId(), notification.getCause());
+    }
+
+    @Override
+    public void onNetworkElementSynchronized(NetworkElementSynchronized notification) {
+        // no op
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.lang.AutoCloseable#close()
+     */
+    @Override
+    public void close() throws Exception {
+        log.debug("CLOSE : {}", Integer.toHexString(this.hashCode()));
+    }
+}
